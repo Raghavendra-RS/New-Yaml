3aaf11048ba2cd2340cdefb78886d619308b33be
Sandeep U Doddamani <sandeepdoddamani84@gmail.com>
2024-09-30 11:17:14 +0530

    new

---
+++ Pages/React.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesPizza.Pages;
+
+public class ReactModel : PageModel
+{
+    private readonly ILogger<ReactModel> _logger;
+
+    public ReactModel(ILogger<ReactModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
